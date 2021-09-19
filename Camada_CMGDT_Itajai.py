0f45c7867c7d0fd44c4b09d40a49e5cc5a02354e
vitorhj <90215531+vitorhj@users.noreply.github.com>
2021-09-19 17:44:41 -0300

    Update Camada_CMGDT_Itajai.py

--- Camada_CMGDT_Itajai.py
+++ Camada_CMGDT_Itajai.py
@@ -17,7 +17,7 @@ import streamlit as st
 
 st.text('teste 1234')
 import pandas as pd
-tabela_deliberacoes = pd.read_excel(os.path.join(folder,'Deliberações CMGDT.xlsx'))
+tabela_deliberacoes = pd.read_excel('Deliberações CMGDT.xlsx')
 st.DataFrame(tabela_deliberacoes)
 #import pandas as pd
 #latlong =pd.read_csv(os.path.join(folder,'plantacadastral','viewplantacadastral_pontos.csv'))
