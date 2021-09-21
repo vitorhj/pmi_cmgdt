326e487e81e034c0412ddb09a052400c9660dfee
vitorhj <90215531+vitorhj@users.noreply.github.com>
2021-09-21 17:21:26 -0300

    Update app_mapa.py

--- app_mapa.py
+++ app_mapa.py
@@ -10,7 +10,7 @@ import geopandas as gpd
 #______________________________________________________________________________________________________________________________________________________
 ##CÓDIGO
 
-deliberacoes = pd.read_csv(r'./dados/CMGDT.csv')
+deliberacoes = pd.read_csv(r'./dados/CMGDT.csv', sep=',')
 st.dataframe(deliberacoes.head())
 
 nova_tabela=pd.read_csv('nova_tabela.csv')
