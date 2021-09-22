1340e823d0d8cd387a2a8e0b5f9c03008f998e04
vitorhj <90215531+vitorhj@users.noreply.github.com>
2021-09-22 18:13:17 -0300

    Update app_mapa.py

--- app_mapa.py
+++ app_mapa.py
@@ -57,6 +57,37 @@ ano_sidebar = st.sidebar.slider('Ano da deliberação:', min_value=2000, max_val
 #st.download_button(label='Download', data = deliberacoes_cmgdt, filename='deliberacoes_cmgdt.csv',mime='csv')
 
 #______________________________________________________________________________________________________________________________________________________
+##FILTRO DOS DADOS A PARTIR DA SIDEBAR
+
+#Colunas da nova tabela
+cadastro = nova_tabela['CADASTRO']
+delib = nova_tabela['Nº DELIBERAÇÃO']
+ano = nova_tabela['DATA']
+protocolo = nova_tabela['PROTOCOLO']
+rsocial = nova_tabela['RAZÃO SOCIAL']
+logradouro = nova_tabela['nomevia']
+
+#Filtro cadastro
+if cadastro_sidebar != '':
+    nova_tabela=nova_tabela[nova_tabela['CADASTRO']==int(cadastro_sidebar)]
+    
+#Filtro deliberação
+if delib_sidebar != '':
+    nova_tabela=nova_tabela[nova_tabela['Nº DELIBERAÇÃO']==delib_sidebar]
+    
+#Filtro protocolo   
+if prot_sidebar != '':
+    nova_tabela=nova_tabela[nova_tabela['PROTOCOLO']==prot_sidebar]
+
+#Filtro razão social
+if razaosocial_sidebar != '':
+    nova_tabela=nova_tabela[nova_tabela['RAZÃO SOCIAL']==razaosocial_sidebar]
+
+#Filtro pelo ano
+#if logradouro_sidebar != '':
+    #st.dataframe(nova_tabela[nova_tabela['RAZÃO SOCIAL']==razaosocial_sidebar])
+
+#______________________________________________________________________________________________________________________________________________________
 ##TRATAMENTO DOS DADOS PARA GEOPANDAS
 
 #Converte os dados da tabela em dados geoespaciais
@@ -118,117 +149,20 @@ m.add_child(folium.LayerControl())
 #m.save(outfile=os.path.join(folder,'map.html'))
 
 #______________________________________________________________________________________________________________________________________________________
-##ESTRUTURA DA PÁGINA (SIDEBAR NÃO PREENCHIDA)
+##ESTRUTURA DA PÁGINA 
 
 ##Título acima do mapa
 st.title('Mapa de deliberações do CMGDT')
 
 ##Mapa com as deliberações CMGDT
-if (cadastro_sidebar=='')&(delib_sidebar=='')&(prot_sidebar=='')&(razaosocial_sidebar=='')&(logradouro_sidebar==''):
-    folium_static(m, width=1420, height=400)
+folium_static(m, width=1420, height=400)
 
 #Tabela com deliberações
-if (cadastro_sidebar=='')&(delib_sidebar=='')&(prot_sidebar=='')&(razaosocial_sidebar=='')&(logradouro_sidebar==''):
-    st.dataframe(deliberacoes_cmgdt, width=1600)
-else:
-    st.text('cadastro ou deliberação preenchidos')
+st.dataframe(nova_tabela)
+
  
 st.text('_________________________________________________________________________________')
 
 #Rodapé da página
 st.text('Prefeitura Municipal de Itajaí - Secretaria de Desenvolvimento Urbano e Habitação')
 st.text('Última atualização dos dados: 22/09/2021')
-
-#______________________________________________________________________________________________________________________________________________________
-##FILTRO DOS DADOS A PARTIR DA SIDEBAR
-
-#Colunas da nova tabela
-cadastro = nova_tabela['CADASTRO']
-delib = nova_tabela['Nº DELIBERAÇÃO']
-ano = nova_tabela['DATA']
-protocolo = nova_tabela['PROTOCOLO']
-rsocial = nova_tabela['RAZÃO SOCIAL']
-logradouro = nova_tabela['nomevia']
-
-#Filtro cadastro
-if cadastro_sidebar != '':
-    nova_tabela=nova_tabela[nova_tabela['CADASTRO']==int(cadastro_sidebar)]
-    
-#Filtro deliberação
-if delib_sidebar != '':
-    nova_tabela=nova_tabela[nova_tabela['Nº DELIBERAÇÃO']==delib_sidebar]
-    
-#Filtro protocolo   
-if prot_sidebar != '':
-    nova_tabela=nova_tabela[nova_tabela['PROTOCOLO']==prot_sidebar]
-
-#Filtro razão social
-if razaosocial_sidebar != '':
-    nova_tabela=nova_tabela[nova_tabela['RAZÃO SOCIAL']==razaosocial_sidebar]
-
-#Filtro pelo ano
-#if logradouro_sidebar != '':
-    #st.dataframe(nova_tabela[nova_tabela['RAZÃO SOCIAL']==razaosocial_sidebar])
-    
-#______________________________________________________________________________________________________________________________________________________
-##ESTRUTURA DA PÁGINA FILTRADA E PLOTAGEM DOS MAPAS E TABELAS COM FITLRO
-
-##Tratamento dos dados Geopandas
-
-#Converte os dados da tabela em dados geoespaciais
-
-ultima_linha = len(nova_tabela)
-
-#Nova variável para cada tabela
-df=nova_tabela.copy()
-df2=latlong.copy()
-
-df['geometry'] = df['geometry'].apply(wkt.loads)
-df2['geometry'] = df2['geometry'].apply(wkt.loads)
-
-gdf = gpd.GeoDataFrame(df, geometry='geometry', crs=4326)
-gdf2 = gpd.GeoDataFrame(df2, geometry='geometry', crs=4326)
-#gdf2.head()
-
-lotes_cmgdt = gdf['geometry']
-todos_lotes = gdf2['geometry']
-
-#MAPAS FILTRADOS
-
-##Insere a geometria dos lotes das empresas aprovadas pelo conselho:
-if (cadastro_sidebar!='') | (delib_sidebar!='') | (prot_sidebar!='') | (razaosocial_sidebar!='') | (logradouro_sidebar!=''):
-    m = folium.Map(location=[-26.9038,-48.6821], zoom_start=14, tiles="cartodbpositron")
-    
-    folium.Choropleth(
-        geo_data=lotes_cmgdt,
-        fill_color='black',
-        fill_opacity=0.3,
-        line_opacity=1,
-    ).add_to(m)
-
-    ## Insere markers com as informções das empresas que foram para conselho
-
-    ultima_linha = len(nova_tabela)
-
-    for i in range(ultima_linha):
-      lat = nova_tabela.iloc[i]['latitude']
-      lon = nova_tabela.iloc[i]['longitude']
-      rsocial = nova_tabela.loc[i]['RAZÃO SOCIAL']
-      prot = nova_tabela.loc[i]['PROTOCOLO']
-      data = nova_tabela.loc[i]['DATA']
-      ndeliberacao = nova_tabela.loc[i]['Nº DELIBERAÇÃO']
-      cadastro = nova_tabela.loc[i]['CADASTRO']
-      endereco = nova_tabela.loc[i]['ENDEREÇO COMPLETO']
-      deliberacao = nova_tabela.loc[i]['DELIBERAÇÃO']
-
-      folium.Marker(
-          location=[lon,lat],
-          popup = 'Protocolo: '+prot+' , Nº Deliberação: '+str(ndeliberacao)+' , Data deliberação: '+str(data)+' , Razão Social: '+rsocial+' , Nº cadastro: '+str(cadastro)+ ', Endereço: '+str(endereco),
-          tooltip = 'Protocolo: '+prot+', Razão Social: '+rsocial,
-          icon=folium.Icon(color="black")
-      ).add_to(m)
-
-    m.add_child(folium.LayerControl())
-
-    st.dataframe(nova_tabela)
-
